135d7986af0ae7e63d1366c7b4f01c363d15848f
Kim1201 <huynhkimdieu1201@gmail.com>
2023-05-10 07:42:00 +0700

    map

--- mystaticfiles/script.js
+++ mystaticfiles/script.js
@@ -17,7 +17,7 @@ const API_KEY ='14ccf478e4f28fd533a07ca06bb234e4';
 
 setInterval(() => {const timeEl = document.getElementById('time');
 const dateEl = document.getElementById('date');
-const currentWeatherItemsEl = document.getElementById('current-weather-items'); git             
+const currentWeatherItemsEl = document.getElementById('current-weather-items');            
 const timezone = document.getElementById('time-zone');
 const countryEl = document.getElementById('country');
 const weatherForecastEl = document.getElementById('weather-forecast');
@@ -46,13 +46,16 @@ setInterval(() => {
 }, 1000);
 
 getWeatherData()
+getDailyWeatherForecastData()
 function getWeatherData () {
     navigator.geolocation.getCurrentPosition((success) => {
         
         let {latitude, longitude } = success.coords;
-
+        let myArray = document.getElementById("select-province").value.split("|");
+        latitude = myArray[1];
+        longitude = myArray[2];
+        console.log(myArray)
         fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`).then(res => res.json()).then(data => {
-
         console.log(data)
         showWeatherData(data);
         })
@@ -68,8 +71,8 @@ function showWeatherData (data){
     let sunset = data.sys.sunset
     let speed = data.wind.speed
 
-    timezone.innerHTML = data.timezone;
-    countryEl.innerHTML = data.lat + 'N ' + data.lon+'E'
+    timezone.innerHTML = ''//data.timezone;
+    countryEl.innerHTML = ''//data.sys.country//data.lat + 'N ' + data.lon+'E'
 
     currentWeatherItemsEl.innerHTML = 
     `<div class="weather-item">
@@ -97,33 +100,6 @@ function showWeatherData (data){
     
     `;
 
-    let otherDayForcast = ''
-    data.daily.forEach((day, idx) => {
-        if(idx == 0){
-            currentTempEl.innerHTML = `
-            <img src="http://openweathermap.org/img/wn//${day.weather[0].icon}@4x.png" alt="weather icon" class="w-icon">
-            <div class="other">
-                <div class="day">${window.moment(day.dt*1000).format('dddd')}</div>
-                <div class="temp">Night - ${day.temp.night}&#176;C</div>
-                <div class="temp">Day - ${day.temp.day}&#176;C</div>
-            </div>
-            
-            `
-        }else{
-            otherDayForcast += `
-            <div class="weather-forecast-item">
-                <div class="day">${window.moment(day.dt*1000).format('ddd')}</div>
-                <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
-                <div class="temp">Night - ${day.temp.night}&#176;C</div>
-                <div class="temp">Day - ${day.temp.day}&#176;C</div>
-            </div>
-            
-            `
-        }
-    })
-
-
-    weatherForecastEl.innerHTML = otherDayForcast;
 }
     const time = new Date();
     const month = time.getMonth();
@@ -140,13 +116,97 @@ function showWeatherData (data){
 
 }, 1000);
 
+
+function getDailyWeatherForecastData () {
+    navigator.geolocation.getCurrentPosition((success) => {
+        
+        let {latitude, longitude } = success.coords;
+        let myArray = document.getElementById("select-province").value.split("|");
+        latitude = myArray[1];
+        longitude = myArray[2];
+        console.log(myArray)
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`).then(res => res.json()).then(data => {
+
+        showDailyWeatherForecastData(data);
+        })
+
+    })
+}
+
+function showDailyWeatherForecastData (data){
+
+    let otherDayForcast = ''
+    data.list.forEach((day, idx) => {
+        let myArrayDateTime = day.dt_txt.split(" ");
+        let myArrayHour = myArrayDateTime[1].split(":");
+        let myArrayDay = myArrayDateTime[0].split("-");
+        if(idx == 0){
+            currentTempEl.innerHTML = `
+                    <img src="http://openweathermap.org/img/wn//${day.weather[0].icon}@4x.png" alt="weather icon" class="w-icon">
+                    <div class="other">
+                        <div class="day">${window.moment(day.dt*1000).format('dddd')}</div>
+                        <div class="temp">${myArrayHour[0]}:${myArrayHour[1]}</div>
+                        <div class="temp">Temperature</div>
+                        <div class="temp">Min - ${day.main.temp_min}&#176;C</div>
+                        <div class="temp">Max - ${day.main.temp_max}&#176;C</div>
+                        <div class="temp-hour">${myArrayDay[2]}-${myArrayDay[1]}-${myArrayDay[0]}</div>
+                    </div>
+                
+                `
+        }
+        if(myArrayHour[0] == '06'){
+            otherDayForcast += `
+                <div class="weather-forecast-item">
+                    <div class="day">${window.moment(day.dt*1000).format('ddd')}</div>
+                    <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+                    <div class="temp-hour">${myArrayHour[0]}:${myArrayHour[1]}</div>
+                    <div class="temp">Min - ${day.main.temp_min}&#176;C</div>
+                    <div class="temp">Max - ${day.main.temp_max}&#176;C</div>
+                    <div class="temp-hour">${myArrayDay[2]}-${myArrayDay[1]}-${myArrayDay[0]}</div>
+                </div>
+                `
+            // if(isFirstWeatherForecast){
+            //     currentTempEl.innerHTML = `
+            //         <img src="http://openweathermap.org/img/wn//${day.weather[0].icon}@4x.png" alt="weather icon" class="w-icon">
+            //         <div class="other">
+            //             <div class="day">${window.moment(day.dt*1000).format('dddd')}</div>
+            //             <div class="temp">${myArrayHour[0]}:${myArrayHour[1]}</div>
+            //             <div class="temp">Temperature</div>
+            //             <div class="temp">Min - ${day.main.temp_min}&#176;C</div>
+            //             <div class="temp">Max - ${day.main.temp_max}&#176;C</div>
+            //         </div>
+                
+            //     `
+            // }else{
+            //     otherDayForcast += `
+            //     <div class="weather-forecast-item">
+            //         <div class="day">${window.moment(day.dt*1000).format('ddd')}</div>
+            //         <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+            //         <div class="temp-hour">${myArrayHour[0]}:${myArrayHour[1]}</div>
+            //         <div class="temp">Min - ${day.main.temp_min}&#176;C</div>
+            //         <div class="temp">Max - ${day.main.temp_max}&#176;C</div>
+            //     </div>
+            //     `
+                
+            // }
+        }
+    }
+    )
+
+
+    weatherForecastEl.innerHTML = otherDayForcast;
+}
+
 getMapIframeData()
 function getMapIframeData(){
-    var province = document.getElementById("select-province").value;
+    let myArray = document.getElementById("select-province").value.split("|");
+    var province = myArray[0];
     fetch(`https://siphoria-api-production.up.railway.app/api/map/test/${province}`).then(res => res.json()).then(data => {
 
         console.log(data)
         showMapIframeData(data);
+        getWeatherData();
+        getDailyWeatherForecastData();
     })
 }
 
@@ -159,7 +219,10 @@ function getWeatherData () {
     navigator.geolocation.getCurrentPosition((success) => {
         
         let {latitude, longitude } = success.coords;
-
+        let myArray = document.getElementById("select-province").value.split("|");
+        latitude = myArray[1];
+        longitude = myArray[2];
+        console.log(myArray)
         fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`).then(res => res.json()).then(data => {
 
         console.log(data)
